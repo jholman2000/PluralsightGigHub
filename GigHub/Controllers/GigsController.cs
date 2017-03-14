46357b1ddadb508ae630abe1bc03007dc4a44396
u191522 <u191522@DTCDB812FF36100>
2017-03-14 15:11:42 -0400

    Add button to Create

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using GigHub.Models;
 using GigHub.ViewModels;
@@ -10,14 +7,13 @@ namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public GigsController()
         {
             _context = new ApplicationDbContext();
         }
 
-        // GET: Gigs
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
