08806233cbd28427ad16b38be59cd04f752cbb71
Jeff Holman <jeff.holman@yahoo.com>
2017-03-28 13:52:47 -0400

    Added Edit

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using GigHub.Controllers;
 using GigHub.Models;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
+        public string Heading { get; set; }
+
+        public string Action {
+            get
+            {
+                // Instead of hardcode like this: return (Id != 0) ? "Update" : "Create";
+                Expression<Func<GigsController, ActionResult>> update = (c => c.Update(this));
+                Expression<Func<GigsController, ActionResult>> create = (c => c.Create(this));
+                var action = (Id != 0) ? update : create;
+
+                return (action.Body as MethodCallExpression).Method.Name;
+            }
+        }
+
         [Required]
         public string Venue { get; set; }
 
